9c70e23881e8055fbbc4f1e9aae7ffd8f20e3167
VolMart <cycleofdelusion@gmail.com>
2021-06-11 23:50:55 +0300

    Diffuse materials and gamma correction

--- utility.h
+++ utility.h
@@ -20,6 +20,11 @@ inline double random_double() {
     return distribution(generator);
 }
 
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
